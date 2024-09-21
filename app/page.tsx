9229dc0a8388ed41ca676ad32931f32a21d09f28
LegioN2004 <milindabarua04@gmail.com>
2024-09-21 12:53:08 +0530

    init committtttttttt

---
+++ app/page.tsx
@@ -0,0 +1,57 @@
+import {
+	BrainCogIcon,
+	EyeIcon,
+	GlobeIcon,
+	MonitorSmartphoneIcon,
+	ServerCogIcon,
+	ZapIcon,
+} from 'lucide-react';
+
+const features = [
+	{
+		name: 'Store your PDF documents',
+		description:
+			'Keep all your pdf files in one place secured and accessible from anywhere, anytime',
+		icon: GlobeIcon,
+	},
+	{
+		name: 'Blazing fast responses',
+		description:
+			'Keep all your pdf files in one place secured and accessible from anywhere, anytime',
+		icon: ZapIcon,
+	},
+	{
+		name: 'Chat memorization',
+		description:
+			'Keep all your pdf files in one place secured and accessible from anywhere, anytime',
+		icon: BrainCogIcon,
+	},
+	{
+		name: 'Interactive pdf viewer',
+		description:
+			'Keep all your pdf files in one place secured and accessible from anywhere, anytime',
+		icon: EyeIcon,
+	},
+	{
+		name: 'Cloud backup',
+		description:
+			'Keep all your pdf files in one place secured and accessible from anywhere, anytime',
+		icon: ServerCogIcon,
+	},
+	{
+		name: 'Responsive design',
+		description:
+			'Keep all your pdf files in one place secured and accessible from anywhere, anytime',
+		icon: MonitorSmartphoneIcon,
+	},
+];
+
+export default function Home() {
+	return (
+		<main className="pt-5">
+			<div className="border border-l-blue-400">
+				<h3>Your </h3>
+			</div>
+		</main>
+	);
+}
